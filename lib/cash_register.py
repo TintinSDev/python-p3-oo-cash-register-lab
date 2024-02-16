949ad1b1c503fd732b0077a5921d480725094848
Martin <mmaina290@gmail.com>
2024-02-16 21:01:16 +0300

    1 test failing

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,29 +1,52 @@
 #!/usr/bin/env python3
 
+
 class CashRegister:
-    def __init__(self, discount=0):
-        self.total = 0
-        self.items = []
-        self.discount = discount
-        self.last_transaction_amount = 0
 
-    def add_item(self, title, price, quantity=1):
-        transaction_amount = price * quantity
-        self.total += transaction_amount
-        self.last_transaction_amount = transaction_amount
-        self.items.extend([title] * quantity)
+  def __init__(self, discount = 0):
+    
+ 
+    
+    self.discount = discount
+    self.total = 0
+    self.items = []
+    self.track_subtotal = []
+ 
+  
+  def add_item(self, title, price, quantity=1):
+      self.title = title
+      self.price = price
+      self.quantity = quantity 
+      
+      self.total += quantity*price
+      
+      self.track_subtotal.append(price*quantity)
 
-    def apply_discount(self):
-        if self.discount > 0:
-            discount_amount = (self.discount / 100) * self.total
-            self.total -= discount_amount
-            print(f"After the discount, the total comes to ${self.total:.2f}.")
-        else:
-            print("There is no discount to apply.")
+    
+      for _ in range(quantity):
+        self.items.append(title)
 
-    def void_last_transaction(self):
-        if self.items:
-            self.total -= self.last_transaction_amount
-            self.items.pop()
-        else:
-            print("No transactions to void.")
+      return self.total
+  
+  
+  def apply_discount(self):
+    if self.discount > 0:
+        discount_amount = (self.discount / 100) * self.total
+        print(f"Discount amount: {discount_amount}")
+        self.total -= discount_amount
+        print(f"After the discount, the total comes to ${self.total:.2f}.")
+    else:
+        print("There is no discount to apply.")
+
+    
+  def void_last_transaction(self):
+    self.total -= self.track_subtotal[-1]
+    return self.total
+
+
+
+new_register = CashRegister()
+new_register.add_item("eggs", 1.99, 2)
+new_register.add_item("tomato", 1.76, 3)
+print(new_register.items)
+    
